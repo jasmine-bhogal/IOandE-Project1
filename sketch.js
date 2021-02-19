5d87479a8b82f90e7ce489af0c80558ba96a1145
jasmine-bhogal <79305109+jasmine-bhogal@users.noreply.github.com>
2021-02-19 07:46:37 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,33 +9,6 @@ let t = "YOU'RE FUNNY — SOMETIMES.";
 function preload() {
     raleway = loadFont('assets/Raleway-Bold.ttf')
 }
-//
-//function setup() {
-//    createCanvas(255, 255);
-//}
-//
-//function draw() {
-//    fill(255, 10);
-//    rect(0, 0, width, height);
-//    if (counter % 80 == 0) {
-//        getData();
-//    }
-//    counter++;
-//}
-//
-//function getData() {
-//    let data;
-//    httpGet(url, 'json', function (response) {
-//        console.log(response);
-//        data = response.last_value;
-////        let resize = map(data, 1, 0, 25, 150);
-////        noStroke();
-////        fill(255, 0, 0);
-////        ellipse(width / 2, height / 2, resize);
-////        console.log(data);
-//    });
-//}
-
 
 //confetti/gravity/physics adapted from https://editor.p5js.org/slow_izzm/sketches/H1fhGJSaX
 //special thanks to assistance from Michael Brzuchalski
